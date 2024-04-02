f3034de34361010474866188aa020d980f3cef73
RamadossElumalai <Ramadossrohit2264@gmail.com>
2024-04-02 22:33:44 +0530

    subject vs behaviour subject example added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,22 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'rxjs-learning';
+  numberSubject = new Subject<number>();
+  numberbehaviorSubject = new BehaviorSubject<number>(10);
+  constructor() {
+    // subject
+    this.numberSubject.subscribe((response) => console.log(response)); 
+    this.numberSubject.next(1);
+    this.numberSubject.subscribe((response) =>
+      console.log(`new subscription`, response)
+    );
+
+    // behaviour subject
+
+    this.numberbehaviorSubject.subscribe((response) => console.log(response));
+    this.numberbehaviorSubject.next(11);
+    this.numberbehaviorSubject.subscribe((response) =>
+      console.log(`new behaviour subscription`, response)
+    );
+  }
 }
